54bc61b093f73ac16fb3719d5aa6a919b2dab68b
subodh765444 <221b396@juetguna.in>
2025-01-23 11:27:04 +0530

    Add files via upload

---
+++ q4/Main.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Two t=new Two(1);
+	}
+}
+
+// Initiall it will throw an error constructor One in class One cannot be applied to given types;
+// Two(int x){
+
+// This issue can be solved by using super(x) in class two constructor. then the output issue
+// In Class One
+// In Class Two
---
+++ q4/Two.java
@@ -0,0 +1,6 @@
+class Two extends One{
+    Two(int x){
+        super(x);
+        System.out.println("In Class Two");
+    }
+}
